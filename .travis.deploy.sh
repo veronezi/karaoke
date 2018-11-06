9fb0cde2ceb37dbc679763c7e0e61c1a0289c3e2
Thiago Veronezi <thiago@veronezi.org>
2018-11-06 18:33:32 -0500

    travis: fix deploy

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -5,12 +5,12 @@ function deploy_java_bin () {
     mvn deploy:deploy-file -DpomFile=$1/pom.xml -Dfile=$1/target/$2-$3.$4 -Dpackaging=$4 -DrepositoryId=bintray-tveronezi-pretests -Durl=https://api.bintray.com/maven/tveronezi/pretests/$2/;publish=1
 }
 
-deploy_java_bin karaoke/api karaoke-api $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin karaoke/cache karaoke-cache $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin karaoke/functional karaoke-functional $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin karaoke/lyrics karaoke-lyrics $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin karaoke/musixmatch karaoke-musixmatch $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin karaoke/webapp karaoke-webapp $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER war
+deploy_java_bin api karaoke-api $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
+deploy_java_bin cache karaoke-cache $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
+deploy_java_bin functional karaoke-functional $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
+deploy_java_bin lyrics karaoke-lyrics $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
+deploy_java_bin musixmatch karaoke-musixmatch $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
+deploy_java_bin webapp karaoke-webapp $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER war
 
 function deploy_docker_image () {
     echo "[RUNNING] docker tag $1 veronezi/$1:$2-b$3"
