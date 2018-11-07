2b60655f61927a4aea1f9383825ea7267f9e63f8
Thiago Veronezi <thiago@veronezi.org>
2018-11-07 13:20:34 -0500

    this projevt has no java binary to deploy

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -e
 
-REPO_ID=bintray-tveronezi-pretests
-REPO_URL=https://api.bintray.com/maven/tveronezi/pretests
-
-function deploy_java_bin () {
-    mvn deploy:deploy-file -DpomFile=$1/pom.xml -Dfile=$1/target/$2-$3.$4 -Dpackaging=$4 -DrepositoryId=$REPO_ID -Durl=$REPO_URL/$2/;publish=1
-}
-
-deploy_java_bin api karaoke-api $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin cache karaoke-cache $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin functional karaoke-functional $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin lyrics karaoke-lyrics $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin musixmatch karaoke-musixmatch $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER jar
-deploy_java_bin webapp karaoke-webapp $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER war
-
 function deploy_docker_image () {
     if [ "$TRAVIS_BRANCH" = "master" ]; then
         echo "deploying docker image veronezi/$1:$2-b$3"
