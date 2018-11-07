30692121e6ae3149492be6e2a387ef507512550a
Thiago Veronezi <thiago@veronezi.org>
2018-11-07 10:26:27 -0500

    handy log messages

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -16,12 +16,14 @@ deploy_java_bin musixmatch karaoke-musixmatch $KARAOKE_VERSION-b$TRAVIS_BUILD_NU
 deploy_java_bin webapp karaoke-webapp $KARAOKE_VERSION-b$TRAVIS_BUILD_NUMBER war
 
 function deploy_docker_image () {
-    if [ "$TRAVIS_PULL_REQUEST" -eq "false" ]; then
+    if [ "$TRAVIS_BRANCH" -eq "master" ]; then
+        echo "deploying docker image veronezi/$1:$2-b$3"
         docker tag $1 veronezi/$1:$2-b$3
         docker push veronezi/$1:$2-b$3
     else
-       docker tag $1 veronezi/$1:$2-pr$TRAVIS_PULL_REQUEST
-       docker push veronezi/$1:$2-pr$TRAVIS_PULL_REQUEST
+        echo "deploying docker image veronezi/$1:$2-rc$3"
+       docker tag $1 veronezi/$1:$2-rc$3
+       docker push veronezi/$1:$2-rc$3
     fi
 }
 
@@ -33,3 +35,5 @@ deploy_docker_image karaoke-static $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
 deploy_docker_image karaoke-static-install $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
 deploy_docker_image karaoke-rendertron $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
 deploy_docker_image karaoke-tomee $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
+
+echo "binaries uploaded"
