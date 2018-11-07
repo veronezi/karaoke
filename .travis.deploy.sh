c1121c0646ebbf3e2135c7d473db83a24573046a
Thiago Veronezi <thiago@veronezi.org>
2018-11-07 15:36:39 -0500

    using servers from external projects

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -5,7 +5,9 @@ function deploy_docker_image () {
     if [ "$TRAVIS_BRANCH" = "master" ]; then
         echo "deploying docker image veronezi/$1:$2-b$3"
         docker tag $1 veronezi/$1:$2-b$3
+        docker tag $1 veronezi/$1
         docker push veronezi/$1:$2-b$3
+        docker push veronezi/$1
     else
         echo "deploying docker image veronezi/$1:$2-rc$3"
        docker tag $1 veronezi/$1:$2-rc$3
@@ -19,7 +21,5 @@ deploy_docker_image karaoke-proxy $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
 deploy_docker_image karaoke-renderer $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
 deploy_docker_image karaoke-static $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
 deploy_docker_image karaoke-static-install $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
-deploy_docker_image karaoke-rendertron $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
-deploy_docker_image karaoke-tomee $KARAOKE_VERSION $TRAVIS_BUILD_NUMBER
 
 echo "binaries uploaded"
--- webapp/src/main/docker/full/app.dockerfile
+++ webapp/src/main/docker/full/app.dockerfile
@@ -1,4 +1,4 @@
-FROM karaoke-tomee
+FROM veronezi/sample-tomee:0.0.1-b5
 WORKDIR /opt/tomee/webapps
 COPY --from=karaoke-war /opt/ /opt/tomee/webapps/ROOT
 COPY --from=karaoke-static /opt/output /opt/tomee/webapps/ROOT/app
