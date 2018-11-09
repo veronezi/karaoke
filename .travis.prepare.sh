66d29a6e984fbf90ae1bf33cadc081a2efcbd7bc
Thiago Veronezi <thiago@veronezi.org>
2018-11-09 11:15:51 -0500

    activate google cloud

--- .travis.prepare.sh
+++ .travis.prepare.sh
@@ -10,3 +10,15 @@ git diff
 
 docker logout
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
+
+if [ ! -d $HOME/google-cloud-sdk/bin ]; then
+    rm -rf $HOME/google-cloud-sdk;
+    curl https://sdk.cloud.google.com | bash > /dev/null;
+fi
+source $HOME/google-cloud-sdk/path.bash.inc
+gcloud --quiet version
+gcloud --quiet components update
+gcloud --quiet components update kubectl
+
+echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
